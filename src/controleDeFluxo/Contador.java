dc50ab8647b380992ff80005f1300716d0130eef
Anderson Gomes <anderson.silva.java@gmail.com>
2025-03-25 11:18:56 -0300

    estrutura de repeticao (Do While) pronta

--- src/controleDeFluxo/Contador.java
+++ src/controleDeFluxo/Contador.java
@@ -6,31 +6,35 @@ public class Contador {
 
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
-		
-		System.out.println(" Digite o primeiro parâmetro: ");
-		int  parametroUm = terminal.nextInt();
-		System.out.println(" Digite o segundo parâmetro: ");
-		int  parametroDois = terminal.nextInt();
-		
-		try {
-			
-			contar(parametroUm, parametroDois);
-			
-		} catch (Exception e ) {
-			System.out.println(e.getMessage());
-			
+		int parametroUm;
+		int parametroDois;
+
+		do {
+
+			try {
+				System.out.println("DIGITE O PRIMEIRO PARAMETRO: ");
+				parametroUm = terminal.nextInt();
+				System.out.println("DIGITE O SEGUNDO PARAMETRO: ");
+				parametroDois = terminal.nextInt();
+
+				contar(parametroUm, parametroDois);
+				break;
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.out.println("TENTE NOVAMENTE ");
+			}
+
+		} while (true);
+	}
+	static void contar(int parametroUm, int parametroDois) throws ParametroInvalidosException {
+		if (parametroUm >= parametroDois) {
+			throw new ParametroInvalidosException("O SEGUNDO PARAMETRO DEVE SER MAIR QUE O PRIMEIRO ");
 		}
 
-	}
-	static void contar (int parametroUm, int parametroDois) throws ParametroInvalidosException{
-		if (parametroUm >= parametroDois) {
-			throw new ParametroInvalidosException(" O SEGUNDO PARAMETRO DEVE SER MAIR QUE O PRIMEIRO ");
-		}
-		
 		int contagem = parametroDois - parametroUm;
 		for (int i = 0; i <= contagem; i++) {
-			System.out.println("Imprimindo os numeros: " + i);
+			System.out.println("Imprimindo os numeros: " + (parametroUm + i));
 		}
 	}
-
 }
